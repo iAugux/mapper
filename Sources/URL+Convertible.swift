e5ef46e014b787b4093228048f182fa1c8c0a067
iAugus <iAugux@gmail.com>
2017-10-30 10:05:23 +0800

    -

--- Sources/URL+Convertible.swift
+++ Sources/URL+Convertible.swift
@@ -1,24 +1,36 @@
 import Foundation
 
-/// URL Convertible implementation
+/**
+ URL Convertible implementation
+ */
 extension URL: Convertible {
-    /// Create a URL from Mapper
-    ///
-    /// - parameter value: The object (or nil) passed from Mapper
-    ///
-    /// - throws: MapperError.convertibleError if the passed value is not a String
-    /// - throws: MapperError.customError      if the passed value is a String but the url initializer fails
-    ///
-    /// - returns: The created url
+    /**
+     Create a URL from Mapper
+
+     - parameter value: The object (or nil) passed from Mapper
+
+     - throws: MapperError.convertibleError if the passed value is not a String
+     - throws: MapperError.customError      if the passed value a String but the URL initializer returns nil
+
+     - returns: The created URL
+     */
     public static func fromMap(_ value: Any) throws -> URL {
         guard let string = value as? String else {
             throw MapperError.convertibleError(value: value, type: String.self)
         }
 
-        if let url = URL(string: string) {
-            return url
+        // Chinese/Korean/Japanese characters can not be part of an URL.
+        if let URL = URL(string: string.encodeURLComponent) {
+            return URL
         }
 
         throw MapperError.customError(field: nil, message: "'\(string)' is not a valid URL")
     }
 }
+
+extension String {
+    fileprivate var encodeURLComponent: String {
+        return addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) ?? self
+    }
+}
+
